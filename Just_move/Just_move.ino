bb0eeeb52f035d8dbe701eef84e01316a7c7f4ae
Ampers8nd <fhncghjvhyu@gmail.com>
2026-03-10 22:30:28 -0400

    Add ultrasonic reader to automate stopping motor

---
+++ Just_move/Just_move.ino
@@ -0,0 +1,20 @@
+#include <myDuino.h>
+myDuino robot(1);
+
+bool triggered = false;
+const float tgt_dist = 7.5;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float us_value = robot.readUltrasonic();
+  if (us_value < 7.5) {
+    robot.moveMotor(1, 2, 0);
+  } else {
+    robot.moveMotor(1,2,200);
+  }
+}
